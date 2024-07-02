f1ad7b4951c0d808e11fbef02edb4b697879e2be
Maryam M El-Kassaby <132655031+Maryam-Elkassaby@users.noreply.github.com>
2024-07-02 20:22:43 +0300

    commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,26 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const [TrendingMovies , setTrendingMovies] = useState([]);
+
   async function getTrendingMovies(){
     let {data} = await axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=f1aca93e54807386df3f6972a5c33b50');
-    console.log(data.results);
+    setTrendingMovies(data.results)
   }
+
   // component didMount
   useEffect(()=>{
     getTrendingMovies();
   } , [])
   return (
     <section className='container'>
-      
+      <div className="row">
+        {TrendingMovies.map((movie)=><div className='col-md-2'>
+          <h2 className='h6'>{movie.title}</h2>
+        </div>)}
+      </div>
     </section>
   )
 }
